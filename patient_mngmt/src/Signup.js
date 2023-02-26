48400419dabecc22a13494d3ed13cbeaede91db7
Aman Dubey <amandubeyrocks847@gmail.com>
2023-02-26 20:42:47 +0530

    add : created signup page

---
+++ patient_mngmt/src/Signup.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Signup()
+{
+   return ( 
+    <div>
+    <h1>
+        Signup
+    </h1>
+        <form>
+            <label>hello</label>
+            <input type="text"/>
+            <input type="password"/>
+        </form> 
+        </div>
+   );
+}
+
+//export default Signup();
